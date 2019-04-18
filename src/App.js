4229ebcd7be431e87cf5903332cf62cf17532d32
Chandradat gir <chandradatgir@yahoo.com>
2019-04-17 20:41:14 -0400

    Added the Shape component & have a working app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
+
+class Selector extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container" >
+        <div className="navbar">
+        <div>Selected:</div>
+        <div>What goes here</div>
+        </div>
+        <div className="shape-list">Add the Shapes here</div>
       </div>
     );
   }
 }
 
-export default App;
+export default Selector;
